9eb32c8b9b03ffd5a41021378278864cbc089ff4
JP Erasmus <jperasmus11@gmail.com>
2016-04-30 22:39:20 +0200

    Add ability to pass an array for either `UserAgent` or `Disallow`

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var fs = require('fs');
 var asArray = require('as-array');
 
+if (!Array.isArray) {
+  Array.isArray = function(arg) {
+    return Object.prototype.toString.call(arg) === '[object Array]';
+  };
+}
+
 module.exports = function(robots) {
   var app = require('express')();
 
@@ -21,7 +27,20 @@ module.exports = function(robots) {
 
 function render(robots) {
   return asArray(robots).map(function(robot) {
-    return ['User-agent: ' + robot.UserAgent].concat(asArray(robot.Disallow).map(function(disallow) {
+    var userAgentArray = [];
+    if (Array.isArray(robot.UserAgent)) {
+      userAgentArray = robot.UserAgent.map(function(userAgent) {
+        return 'User-agent: ' + userAgent
+      });
+    } else {
+      userAgentArray.push('User-agent: ' + robot.UserAgent);
+    }
+    return userAgentArray.concat(asArray(robot.Disallow).map(function(disallow) {
+      if (Array.isArray(disallow)) {
+        return disallow.map(function(line) {
+          return 'Disallow: ' + line;
+        }).join('\n');
+      }
       return 'Disallow: ' + disallow;
     })).join('\n');
   }).join('\n');
