016a50c82f19f4f96feea174baa325aa79c1efab
Sam Scherer <sam.scherer@thedailybeast.com>
2019-04-15 12:49:19 -0400

    Adding sitemaps.

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-var fs = require('fs');
-var asArray = require('as-array');
+const fs = require('fs');
+const asArray = require('as-array');
 
 if (!Array.isArray) {
-  Array.isArray = function(arg) {
+  Array.isArray = function (arg) {
     return Object.prototype.toString.call(arg) === '[object Array]';
   };
 }
 
-module.exports = function(robots) {
-  var app = require('express')();
+module.exports = function (robots) {
+  const app = require('express')();
 
-  if(robots) {
-    robots = 'string' === typeof robots
+  if (robots) {
+    robots = typeof robots === 'string'
       ? fs.readFileSync(robots, 'utf8')
       : render(robots);
-  } else
-    robots = '';
+  } else { robots = ''; }
 
-  app.get('/robots.txt', function(req, res) {
+  app.get('/robots.txt', (req, res) => {
     res.header('Content-Type', 'text/plain');
     res.send(robots);
   });
@@ -25,26 +24,43 @@ module.exports = function(robots) {
   return app;
 };
 
+function disallowResult(Disallow) {
+  return asArray(Disallow).map((disallow) => {
+    if (Array.isArray(disallow)) {
+      return disallow.map((line) => {
+        return `Disallow: ${line}`;
+      }).join('\n');
+    }
+    return `Disallow: ${disallow}`;
+  });
+}
+
 function render(robots) {
-  return asArray(robots).map(function(robot) {
-    var userAgentArray = [];
+  return asArray(robots).map((robot) => {
+    let userAgentArray = [];
+    let siteMapArray = [];
+
     if (Array.isArray(robot.UserAgent)) {
-      userAgentArray = robot.UserAgent.map(function(userAgent) {
-        return 'User-agent: ' + userAgent
+      userAgentArray = robot.UserAgent.map((userAgent) => {
+        return `User-agent: ${userAgent}`;
       });
     } else {
-      userAgentArray.push('User-agent: ' + robot.UserAgent);
+      userAgentArray.push(`User-agent: ${robot.UserAgent}`);
     }
+
     if (robot.CrawlDelay) {
-      userAgentArray.push('Crawl-delay: ' + robot.CrawlDelay);
+      userAgentArray.push(`Crawl-delay: ${robot.CrawlDelay}`);
     }
-    return userAgentArray.concat(asArray(robot.Disallow).map(function(disallow) {
-      if (Array.isArray(disallow)) {
-        return disallow.map(function(line) {
-          return 'Disallow: ' + line;
-        }).join('\n');
-      }
-      return 'Disallow: ' + disallow;
-    })).join('\n');
+
+    if (Array.isArray(robot.Sitemap)) {
+      siteMapArray = robot.Sitemap.map((siteMap) => {
+        return `Sitemap: ${siteMap}`;
+      });
+    } else {
+      siteMapArray.push(`Sitemap: ${robot.Sitemap}`);
+    }
+
+    return userAgentArray.concat(disallowResult(robot.Disallow), siteMapArray).join('\n');
   }).join('\n');
 }
+
