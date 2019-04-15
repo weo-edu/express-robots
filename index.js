765b146f992a12ac37f32b4b7cb4d308860cbfee
Sam Scherer <sam.scherer@thedailybeast.com>
2019-04-15 14:16:51 -0400

    0.2.0
    simplify and preserve es5 syntax

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-const fs = require('fs');
-const asArray = require('as-array');
+var fs = require('fs');
+var asArray = require('as-array');
 
 if (!Array.isArray) {
-  Array.isArray = function (arg) {
+  Array.isArray = function(arg) {
     return Object.prototype.toString.call(arg) === '[object Array]';
   };
 }
 
 module.exports = function (robots) {
-  const app = require('express')();
+  var app = require('express')();
 
   if (robots) {
-    robots = typeof robots === 'string'
+    robots = 'string' === typeof robots
       ? fs.readFileSync(robots, 'utf8')
       : render(robots);
-  } else { robots = ''; }
+  } else
+    robots = '';
 
-  app.get('/robots.txt', (req, res) => {
+  app.get('/robots.txt', function(req, res) {
     res.header('Content-Type', 'text/plain');
     res.send(robots);
   });
@@ -24,45 +25,42 @@ module.exports = function (robots) {
   return app;
 };
 
-function disallowResult(Disallow) {
-  return asArray(Disallow).map((disallow) => {
-    if (Array.isArray(disallow)) {
-      return disallow.map((line) => {
-        return `Disallow: ${line}`;
-      }).join('\n');
-    }
-    return `Disallow: ${disallow}`;
-  });
-}
-
 function render(robots) {
-  return asArray(robots).map((robot) => {
-    let userAgentArray = [];
-    let siteMapArray = [];
-
+   return asArray(robots).map(function(robot) {
+    var userAgentArray = [];
+    var siteMapArray = [];
+    var disallowArray = asArray(robot.Disallow)
+      .map(function(disallow) {
+        if (Array.isArray(disallow)) {
+          return disallow.map(function(line) {
+            return 'Disallow: ' + line;
+          }).join('\n');
+        }
+        return 'Disallow: ' + disallow;
+      });
+   
     if (Array.isArray(robot.UserAgent)) {
-      userAgentArray = robot.UserAgent.map((userAgent) => {
-        return `User-agent: ${userAgent}`;
+      userAgentArray = robot.UserAgent.map(function(userAgent) {
+        return 'User-agent: ' + userAgent
       });
     } else {
-      userAgentArray.push(`User-agent: ${robot.UserAgent}`);
+      userAgentArray.push('User-agent: ' + robot.UserAgent);
     }
-
     if (robot.CrawlDelay) {
-      userAgentArray.push(`Crawl-delay: ${robot.CrawlDelay}`);
+      userAgentArray.push('Crawl-delay: ' + robot.CrawlDelay);
     }
 
     if (robot.Sitemap) {
       if (Array.isArray(robot.Sitemap)) {
-        siteMapArray = robot.Sitemap.map((siteMap) => {
-          return `Sitemap: ${siteMap}`;
+        siteMapArray = robot.Sitemap.map(function(siteMap) {
+          return 'Sitemap: ' + siteMap;
         });
       } else {
-        siteMapArray.push(`Sitemap: ${robot.Sitemap}`);
+        siteMapArray.push('Sitemap: ' + robot.Sitemap);
       }
     }
 
-    return userAgentArray.concat(disallowResult(robot.Disallow), siteMapArray).join('\n');
+    return userAgentArray.concat(disallowArray, siteMapArray).join('\n');
   }).join('\n');
 }
 
